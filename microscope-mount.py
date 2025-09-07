f2ec5dca48ec68c186c819beda267b7388f5b5d5
Maciej Małecki <me@mmalecki.com>
2025-09-07 19:27:36 +0200

    New movement system

--- microscope-mount.py
+++ microscope-mount.py
@@ -1,131 +1,141 @@
 import cadquery as cq
+from ocp_vscode import *
+from cq_queryabolt import boltData, nutData
+from settings import Settings
+from workplane import Workplane
 
-fit = 0.1
+loose_fit = Settings.loose_fit
+bearing_od = 16
+bearing_t = 5
+bearing_id = 5
 
-# Chamfer
-c = 0.5
+scope_d = Settings.scope_d
+bolt_d = boltData(Settings.frame_bolt)['diameter']
+wall_t = Settings.wall_t
+v_slot_d = Settings.v_slot_d
+d = scope_d + 2 * wall_t
 
-slider_bolt_x_s = 38.3509
-slider_bolt_y_s = 20
-slider_bolt_z_s = 20
+rod = "M5"
+rod_d = boltData(rod)['diameter']
+rod_nut = nutData(rod)
+rod_nut_t  = rod_nut['thickness']
+rod_nut_w = rod_nut['width']
 
-padding = 5
-thickness = 5
+holder_bolt = "M3"
+holder_bolt_d = boltData(holder_bolt)['diameter']
 
-mount_w = slider_bolt_x_s + 2 * padding
-mount_h = slider_bolt_z_s + 2 * padding
-mount_holder_lip = 2.5
+dovetail_neck_l = 75
+dovetail_neck_w = 3 * wall_t
+dovetail_neck_t = rod_nut_t + 2 * wall_t
 
-bolt = 3
-bolt_cbore = 5.5
+running_fit = 0.121
 
-scope_d = 50
-holder_t = 7.5
-holder_h = 15
-holder_bolt = 3
-holder_bolt_offset = 5.5
-holder_od = scope_d + 2 * holder_t
-holder_arm_l = 45
+heatset_d = 3.2
+heatset_l = 4
+heatset_wall_t = 1.3
 
-t_w = slider_bolt_x_s - 2 * padding
-t_t = 2 * padding
-t_h = t_t + t_t
+dovetail_wall_t = heatset_d + heatset_wall_t * 2
 
-box_l = t_h + t_t
-box_h = mount_h
+slider_bolt = "M2.5"
+slider_bolt_s = 35 # Mostly arbirtrary, a nice round number that fits in with design rules
+slider_bolt_d = boltData(slider_bolt)['diameter']
+
+cable_t = 1.2
+cable_w = 16
+cable_passthru_t = 2 * wall_t
+
+slider_id = v_slot_d + loose_fit
+slider_od = slider_id + 2 * wall_t
+rod_v_slot_clearance = rod_nut_w / 2 + (bearing_od / 2 - rod_nut_w / 2) + cable_passthru_t
+slider_nutcatch_l = rod_v_slot_clearance + rod_nut_w / 2 + wall_t
+slider_tab_t = (loose_fit - running_fit) / 2
+nut_m_w = rod_nut_w / 2 + wall_t
+bearing_wall_t = wall_t
+
+slider_shape = (cq.Sketch()
+    .polygon([
+        (-slider_od / 2, -slider_od / 2), (slider_od / 2, -slider_od / 2),
+        (slider_od / 2, slider_od / 2), (nut_m_w, slider_od / 2 + rod_v_slot_clearance + nut_m_w),
+        (-nut_m_w, (slider_od / 2 + rod_v_slot_clearance + nut_m_w)),(-slider_od / 2, slider_od / 2), 
+             ])
+                .rect(slider_id, slider_id, mode='s')
+                .rarray(slider_id / 2, slider_id - slider_tab_t, 2, 2)
+                .rect(v_slot_d / 4, slider_tab_t)
+                .reset()
+                .rarray(slider_id - slider_tab_t , slider_id / 2, 2, 2)
+                .rect(slider_tab_t,v_slot_d / 4)
+                .reset()
+                .push([(0, (slider_od + cable_passthru_t) / 2)])
+                .slot(cable_w + loose_fit, cable_t, 0, mode='s')
+                .push([(0, -(slider_od  - wall_t)/2)])
+                .rect(slider_bolt_s + slider_bolt_d + wall_t, wall_t)
+                )
+
+holder_shape = (cq.Sketch()
+                .rect(slider_bolt_s + slider_bolt_d + wall_t, wall_t)
+                .push([(0, -wall_t / 2- dovetail_neck_l / 2)])
+                .rect(dovetail_neck_w, dovetail_neck_l)
+                .push([(0, -dovetail_neck_l - scope_d / 2 - wall_t)])
+                .circle(scope_d / 2 + wall_t)
+                .circle(scope_d / 2, mode='s').reset())
+
+bearing_m_d = bearing_od / 2 + wall_t
+top_shape = (cq.Sketch()
+    .polygon([
+        (-slider_od / 2, -slider_od / 2), (slider_od / 2, -slider_od / 2),
+        (slider_od / 2, slider_od / 2), (bearing_m_d, slider_od / 2 + rod_v_slot_clearance + bearing_m_d),
+        (-bearing_m_d, (slider_od / 2 + rod_v_slot_clearance + bearing_m_d)),(-slider_od / 2, slider_od / 2), 
+             ])
+    .circle((boltData(Settings.frame_bolt)['diameter'] + loose_fit) / 2, mode='s')
+    .push([(0, (slider_od + cable_passthru_t) / 2)])
+    .slot(cable_w, cable_t, 0, mode='s'))
 
 
-def t_shape(workplane, fit=0):
-    pts = [
-        (0, 0),
-        (t_t / 2 + fit / 2, 0),
-        (t_t / 2 + fit / 2, t_h - t_t - fit / 2),
-        (t_w / 2 + fit / 2, t_h - t_t - fit / 2),
-        (t_w / 2 + fit / 2, t_h + fit / 2),
-        (0, t_h + fit / 2),
-    ]
-    return workplane.polyline(pts).mirrorY()
+def top():
+    top = Workplane("XY")
+    top = top.placeSketch(top_shape).extrude(bearing_t + bearing_wall_t).faces(">Z").workplane().move(0, rod_v_slot_clearance + slider_od / 2).tag("bearing").hole(bearing_od, depth=bearing_t)
+    top = top.workplaneFromTagged("bearing").move(0, rod_v_slot_clearance + slider_od / 2).boltHole(rod, clearance=loose_fit)
+    top = top.faces("<Z").workplane().tag("bottom").rect(slider_od, slider_od).extrude(wall_t) # Indexing feature
 
+    top = top.faces("<Z").workplane().move(0, -wall_t).rect(slider_id, slider_id + wall_t * 2).cutBlind(-wall_t)
+    top = top.faces(">Z[1]").edges("|X").edges(cq.selectors.LengthNthSelector(0)).chamfer(wall_t / 2)
 
-def t(workplane, h, fit=0):
-    return t_shape(workplane, fit).extrude(h + fit)
+    # Top mount
+    top = top.edges(">Z and (not %Circle)").chamfer(bearing_t / 4)
+    top = top.edges("|Z and (>>Y or <<Y)").fillet(bearing_od / 4)
+    top = top.edges("<Z").chamfer(wall_t / 4)
+    top = top.faces(">Z[1]").edges(">Y").chamfer(wall_t / 2)
 
+    return top
 
-def mount():
-    result = cq.Workplane("XZ").box(mount_w, mount_h, thickness)
+def slider():
+    slider = Workplane("XY")
+    slider = slider.placeSketch(slider_shape).extrude(dovetail_neck_t)
+    slider = slider.edges("|Z and >Y").fillet(bearing_od / 4)
 
-    plate = result.faces("<Y").workplane()
+    slider = slider.workplane().center(0, slider_od / 2 + rod_v_slot_clearance).tag("rod").workplane(dovetail_neck_t / 2).boltHole(rod, clearance=loose_fit)
+    slider = slider.workplaneFromTagged("rod").workplane(-rod_nut_t / 2).nutcatchSidecut(rod)
+    slider = slider.faces(">Y[1]").workplane(centerOption="CenterOfBoundBox").tag("mount_back").rarray(slider_bolt_s / 2, 1, 3, 1).nutcatchParallel(slider_bolt)
+    slider = slider.workplaneFromTagged("mount_back").rarray(slider_bolt_s / 2, 1, 3, 1).boltHole(slider_bolt, clearance=loose_fit)
 
-    # Aligning these was not as easy as I'd hoped, but there's probably a better way.
-    box = (
-        cq.Workplane("XZ")
-        .transformed(offset=(0, -(mount_h - box_h) / 2, 0))
-        .rect(t_w + 2 * t_t, box_h)
-        .extrude(box_l)
-        .edges("|Z")
-        .chamfer(c)
-    )
-    t_fit = t(
-        cq.Workplane("XY").transformed(offset=(0, -box_l, -mount_h / 2 + t_t - fit)),
-        box_h - t_t,
-        fit=fit,
-    )
-    box = box.cut(t_fit).edges(">Z").chamfer(c)
+    slider = slider.edges("(>Z or <Z) and (not %Circle)").chamfer(wall_t / 4)
 
-    plate = plate.union(box)
-    plate.faces(">Y[2]").tag("holder_mount").end()
-
-    plate = (
-        plate.faces("<Y")
-        .workplane()
-        .rect(slider_bolt_x_s, slider_bolt_z_s, forConstruction=True)
-        .vertices()
-        .hole(bolt, box_l + thickness)
-    )
-    return plate
-
+    return slider
 
 def holder():
-    result = (
-        cq.Workplane("XY")
-        .circle(holder_od / 2)
-        .circle(scope_d / 2)
-        .extrude(holder_h)
-        .tag("holder")
-    )
+    holder = Workplane("XY")
+    holder = holder.placeSketch(holder_shape).extrude(dovetail_neck_t)
+    holder = holder.faces("<Y[1]").workplane(centerOption="CenterOfBoundBox").rarray(slider_bolt_s, 1, 2, 1).boltHole(slider_bolt, clearance=Settings.fit, depth=wall_t)
+    holder = holder.faces(">X[1] or <X[1]").edges("<<Y").fillet(dovetail_neck_w)
+    holder = holder.faces(">X[1] or <X[1]").edges(">>Y").fillet(dovetail_neck_w / 2)
+    holder = holder.faces(">Y").workplane().transformed((0, 0, 0), offset=(0, 0, -d * 1.5)).slot2D(d * 3 / 4, holder_bolt_d + Settings.loose_fit).cutBlind(-d / 2)
 
-    result = t(result.center(0, holder_od / 2 + holder_arm_l), holder_h)
-    result = (
-        result.faces(">Y")
-        .workplane()
-        .rect(t_t, holder_h, centered=[True, False, True])
-        .extrude(until="next")
-    )
+    return holder
 
-    result.faces(">Y").tag("mount")
-
-    # This mounting slot covers ~60 degrees at most. A mounting point for the lens
-    # happens every 90 degrees, so we're essentially hoping for the best here.
-    result = (
-        result.workplaneFromTagged("holder")
-        .center(0, -holder_od / 2)
-        .transformed((90, 0, 90), offset=(0, 0, holder_h - holder_bolt_offset))
-        .slot2D(holder_od / 2, holder_bolt, 90).cutBlind(until = "next")
-    )
-
-    return result.edges(">Z").chamfer(c).edges("<Z").chamfer(c)
-
-
-assembly = (
-    cq.Assembly()
-    .add(mount(), name="mount")
-    .add(holder(), name="holder")
-    .constrain("mount?holder_mount", "holder@faces@>Y", "Plane")
-    .constrain("mount@faces@>Z[1]", "holder@faces@<Z", "Axis")
-)
-
-assembly.solve()
-
-show_object(mount().translate((mount_w + 10, 0)), {"name": "microscope-mount"})
-show_object(holder().translate((mount_w + 10, -150)), {"name": "microscope-holder"})
-show_object(assembly, {"name": "microscope-mount-assembly"})
+holder_ = holder()
+slider_ = slider()
+top_ = top()
+show(holder_.translate((0, -(slider_od + wall_t) / 2, 0)), slider_, top_.translate((0, 0, dovetail_neck_t + wall_t)))
+holder_.export("microscope-mount-holder.step")
+slider_.export("microscope-mount-slider.step")
+top_.export("microscope-mount-top.step")
