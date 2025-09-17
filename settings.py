910ca36f7eef8239ac738af199fb7cef9eae8bc2
Maciej Małecki <me@mmalecki.com>
2025-09-17 19:58:49 +0200

    Add some common fastener definitions

--- settings.py
+++ settings.py
@@ -1,7 +1,19 @@
+from cq_queryabolt import boltData
+
 class Settings:
     tight_fit = 0.1
     fit = 0.2
     loose_fit = 0.5
 
+    bolt_fit = fit
+
     v_slot_d = 20
+    wall_t = 4
+
+    frame_bolt = "M5"
+    frame_bolt_d = boltData(frame_bolt)['diameter']
+
     v_slot_bolt = "M4"
+    v_slot_bolt_d = boltData(v_slot_bolt)['diameter']
+
+    scope_d = 50
